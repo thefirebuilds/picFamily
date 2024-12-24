c702655dd716cf595918b87a7de5078eccd4ae09
thefirebuilds <93689673+thefirebuilds@users.noreply.github.com>
2024-12-24 17:32:53 -0600

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#cleanup log files and images.
+
+# Remove specific files
+rm -f /home/pi/fbi_debug.log
+rm -f /home/pi/fim_log.txt
+rm -f /home/pi/install.sh
+
+# Remove image files with specified extensions
+find /home/pi -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.gif" \) -exec rm -f {} \;
+
+# Remove the cron_output.log file
+rm -f /home/pi/cron_output.log
