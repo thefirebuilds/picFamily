eff7c4e04b93d3e8aa9f27e24810933298e16893
thefirebuilds <93689673+thefirebuilds@users.noreply.github.com>
2024-12-24 12:46:29 -0600

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo  "Preparing PicFamily"
+sleep 90
+
+until [ -e /dev/fb0 ]; do
+	echo "Waiting for framebuffer device..." >> /home/pi/fbi_debug.log
+	sleep 1
+done
+echo "/dev/fb0 ready" >> /home/fbi/fbi_debug.log
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export TERM=xterm
+
+URI="http://192.168.86.167:3000"
+
+echo "fetching data from $URI"
+
+currentPic=$(curl -s "$URI/settings" | json currentPic)
+echo "$currentPic"
+
+fullPath="$URI/images/$currentPic"
+
+echo "$fullPath"
+
+wget -q -O "/home/pi/$currentPic" "$fullPath"
+sleep 5
+
+echo "loading:  /home/pi/$currentPic"
+
+#clear fim buffers
+pkill fim
+clear > /dev/fb0
+
+setterm -cursor off
+sleep 1
+sudo fim -A -q -T 1 -d /dev/fb0 "/home/pi/$currentPic" > fim_log.txt 2>&1 && exit  
+setterm -cursor on
