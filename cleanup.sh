731cd82ef304c0745ca977bf6d145e78912199f2
thefirebuilds <93689673+thefirebuilds@users.noreply.github.com>
2025-01-09 15:04:59 -0600

    Update cleanup.sh
    add removal of debug log

--- cleanup.sh
+++ cleanup.sh
@@ -5,6 +5,7 @@
 rm -f /home/pi/fbi_debug.log
 rm -f /home/pi/fim_log.txt
 rm -f /home/pi/install.sh
+rm -f /home/pi/picfamily_debug.log
 
 # Remove image files with specified extensions
 find /home/pi -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.gif" \) -exec rm -f {} \;
